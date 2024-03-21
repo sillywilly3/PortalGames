2c8823721c3542a27a58432092daa153de06a800
sillywilly3 <130715962+sillywilly3@users.noreply.github.com>
2024-03-21 11:19:23 -0500

    Update home.js
    mobile not supported lol

--- home.js
+++ home.js
@@ -14,3 +14,16 @@ function toggleCloak() {
         applyCloak();
     }
 }
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    window.location.href = "/device-not-supported.html";
+} else {
+    var games = document.querySelectorAll('.game');
+
+    games.forEach(function(game) {
+        game.addEventListener('click', function() {
+            var id = this.id;
+            var url = id + '.html';
+            window.location.href = url;
+        });
+    });
+}
